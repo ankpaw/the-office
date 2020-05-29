9be075ae737320f22dc9954d6e6e7f3cfc5719d1
Ankit Pawar <officiallyhuman@gmail.com>
2020-05-29 22:24:17 +0530

    Use sass variables

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,6 +1,8 @@
 import { AvatarComponent } from "./avatar/avatar.component";
 import { CatalogComponent } from "./catalog/catalog.component";
-export const components: any[] = [AvatarComponent, CatalogComponent];
+import { TileComponent } from "./tile/tile.component";
+export const components: any[] = [AvatarComponent, CatalogComponent, TileComponent];
 
 export * from "./avatar/avatar.component";
 export * from "./catalog/catalog.component";
+export * from "./tile/tile.component";
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as shared from './components';
 import { CatalogComponent } from './components/catalog/catalog.component';
+import { TileComponent } from './components/tile/tile.component';
 
 @NgModule({
-  declarations: [...shared.components, CatalogComponent],
+  declarations: [...shared.components, CatalogComponent, TileComponent],
   imports: [
     CommonModule
   ],
