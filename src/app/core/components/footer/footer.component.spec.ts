8da0bcc7f7cf13bc3ad020f616fa59a1d55e19de
Ankit Pawar <officiallyhuman@gmail.com>
2020-05-31 01:45:17 +0530

    Updated UTs

--- src/app/core/components/footer/footer.component.spec.ts
+++ src/app/core/components/footer/footer.component.spec.ts
@@ -8,9 +8,9 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [FooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +22,19 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain desired text', () => {
+    const fixture = TestBed.createComponent(FooterComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.boring-footer .container .footer-text').textContent).toContain('MAQ Software Confidential');
+  });
+
+  describe('getCurrentDate', () => {
+    it('should return current date', () => {
+      let expectedOutcome = new Date();
+      let actualOutcome = component.getCurrentDate();
+      expect(actualOutcome).toEqual(expectedOutcome);
+    });
+  })
 });
--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -12,4 +12,8 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public getCurrentDate(): Date {
+    return new Date();
+  }
+
 }
